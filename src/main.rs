7c5512d72637e06657b02e8ee7239ca999a0459f
Pablo Lanza <pablolanza89@gmail.com>
2022-10-23 01:19:15 +0100

    began defining the grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod parser;
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    parser::parse("examples/ex0.otrs")?;
+
+    Ok(())
 }
