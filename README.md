021cd97be09814922ef427e32413f411b3a27376
Pablo Lanza <pablolanza89@gmail.com>
2022-12-29 18:38:09 +0100

    Revert "Reconfigured to workspace"
    This reverts commit 85e6b64adb8eb2c826663813e4bf643d81167e4e.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Oters
+
+Oxidized Temporal Reactive Streams
+A Functional Reactive Programming language for intuitively building GUIs.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
 #![allow(dead_code)]
 
-pub mod export;
 pub mod exprs;
 pub mod interpret;
 pub mod parser;
 pub mod types;
+
+use anyhow::Result;
+use interpret::Interpreter;
+use types::check::ProgramChecker;
+
+fn main() -> Result<()> {
+    let program = parser::parse_file("examples/ex2.otrs".to_string())?;
+    let mut checker = ProgramChecker::new();
+
+    let exprs = checker.type_check_program(&program)?;
+
+    let mut interpreter = Interpreter::new();
+    interpreter.init(exprs)?;
+    interpreter.eval_loop()?;
+
+    Ok(())
+}
